ed44f851330550662edf4c714e119edbc254a81b
darkend16987 <hoangnam.mng@gmail.com>
2026-03-08 20:57:26 +0700

    update landing

--- landing-page/public/firebase-config.js
+++ landing-page/public/firebase-config.js
@@ -11,14 +11,15 @@
  */
 
 const firebaseConfig = {
-    // TODO: Replace with your Firebase project configuration
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyAGyNj7mTriivueAQWs-OM2Ug35mqAWjBc",
+    authDomain: "s2rtool-e38ff.firebaseapp.com",
+    projectId: "s2rtool-e38ff",
+    storageBucket: "s2rtool-e38ff.firebasestorage.app",
+    messagingSenderId: "609593402839",
+    appId: "1:609593402839:web:752114cc630e0dd03d9792",
+    measurementId: "G-7N8TTFW7J0"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
